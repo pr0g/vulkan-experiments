ecbdeccb181e90f3ae6854e137de5baf995ee2c8
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-02-10 21:41:11 +0000

    update how third-party libraries are accessed

--- configure.sh
+++ configure.sh
@@ -1,11 +1,13 @@
 cmake -S . -B build/debug -G Ninja \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 -DCMAKE_BUILD_TYPE=Debug \
--DCMAKE_PREFIX_PATH=$(pwd)/third-party/sdl/build \
+-DCMAKE_PREFIX_PATH="$(pwd)/third-party/sdl/build;"\
+"$(pwd)/third-party/tinyobjloader/build" \
 -DAS_COL_MAJOR=ON -DAS_PRECISION_FLOAT=ON
 
 cmake -S . -B build/release -G Ninja \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_PREFIX_PATH=$(pwd)/third-party/sdl/build \
+-DCMAKE_PREFIX_PATH="$(pwd)/third-party/sdl/build;"\
+"$(pwd)/third-party/tinyobjloader/build" \
 -DAS_COL_MAJOR=ON -DAS_PRECISION_FLOAT=ON
